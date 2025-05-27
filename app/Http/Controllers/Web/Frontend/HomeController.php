21eac7a54657c82b7618c96083256074c7009f40
unknown <alauddinnajim202@gmail.com>
2025-05-27 10:00:15 +0600

    fix: Update HomeController methods and routes for consistency

--- app/Http/Controllers/Web/Frontend/HomeController.php
+++ app/Http/Controllers/Web/Frontend/HomeController.php
@@ -12,16 +12,9 @@ use function Symfony\Component\String\b;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function create()
     {
-        $cms = [
-            'home' => CMS::where('page', PageEnum::HOME)->where('status', 'active')->get(),
-            'common' => CMS::where('page', PageEnum::COMMON)->where('status', 'active')->get(),
-        ];
-        
-        $posts = Post::where('status', 'active')->paginate(9);
-
-        return view('frontend.layouts.index', compact('cms', 'posts'));
+        return view('auth.login');
     }
 
     public function post($slug){
