13067877192f1f56696b6b8ce5a091ae93219ba9
unknown <alauddinnajim202@gmail.com>
2025-06-23 12:20:02 +0600

    fix: Correct category model reference in Recipe model

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -23,7 +23,7 @@ class Recipe extends Model
 
     public function category()
     {
-        return $this->hasMany(Category::class);
+        return $this->belongsTo(Category::class, 'category_id', 'id');
     }
 
     public function instructions()
